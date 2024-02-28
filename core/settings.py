5720dd8a79ad03a848eae2c22f953cd0760d436b
George Dudaev <georgedudaev@MacBook-Air-George.local>
2024-02-28 16:36:34 +0300

    add origins

--- core/settings.py
+++ core/settings.py
@@ -41,10 +41,11 @@ CORS_ALLOW_METHODS = [
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://127.0.0.1:3000',
+# ]
+CORS_ALLOWED_ORIGINS = ['*']
 
 INTERNAL_IPS = [
     "127.0.0.1",
